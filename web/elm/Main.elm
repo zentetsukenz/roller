4430ddc95a0fca9ab46fc332884d636d6967f1f2
Wiwatta  Mongkhonchit <zentetsukenz@gmail.com>
2016-10-10 01:33:09 +0700

    Restore Main and add Routing

--- web/elm/Main.elm
+++ web/elm/Main.elm
@@ -1,58 +1,10 @@
 module Main exposing (..)
 
-import Navigation
+import Html exposing (..)
 
 
-main : Program Never
+main : Html msg
 main =
-    Navigation.program (Navigation.makeParser State.pathParser)
-        { init = init
-        , view = view
-        , update = update
-        , urlUpdate = urlUpdate
-        , subscriptions = subscriptions
-        }
-
-
-
--- MODEL
-
-
-type Page
-    = Dashboard
-
-
-type alias Model =
-    { page : Page }
-
-
-init : Result String Int -> ( Model, Cmd Msg )
-init result =
-    urlUpdate result 0
-
-
-
--- UPDATE
-
-
-type Msg
-    = NavigateTo Page
-
-
-urlUpdate : Result String Int -> Model -> ( Model, Cmd Msg )
-urlUpdate result model =
-    case result of
-        Ok newCount ->
-            ( newCount, Cmd.none )
-
-        Err _ ->
-            ( model, Navigation.modifyUrl (toUrl model) )
-
-
-
--- SUBSCRIPTIONS
-
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.none
+    h1
+        []
+        [ text "Let's roll!" ]
