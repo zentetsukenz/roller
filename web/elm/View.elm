243ef6f9141bb59317fe1e8df17941ed40a546e1
Wiwatta  Mongkhonchit <zentetsukenz@gmail.com>
2016-10-16 15:48:43 +0700

    Add simple navbar

--- web/elm/View.elm
+++ web/elm/View.elm
@@ -1,6 +1,6 @@
 module View exposing (..)
 
-import Html exposing (Html, div, text)
+import Html exposing (Html, div, text, li, ul)
 import Models exposing (..)
 import Routing exposing (..)
 import Messages exposing (Msg(..))
@@ -8,7 +8,10 @@ import Messages exposing (Msg(..))
 
 view : Model -> Html Msg
 view model =
-    div [] [ page model ]
+    div []
+        [ div [] [ navBar model ]
+        , div [] [ page model ]
+        ]
 
 
 page : Model -> Html Msg
@@ -22,3 +25,25 @@ page model =
 
         NotFoundRoute ->
             div [] [ text "Not Found!!" ]
+
+
+navBar : Model -> Html Msg
+navBar model =
+    case model.route of
+        DashboardRoute ->
+            ul []
+                [ li [] [ text "At Dashboard" ]
+                , li [] [ text "About" ]
+                ]
+
+        AboutRoute ->
+            ul []
+                [ li [] [ text "Dashboard" ]
+                , li [] [ text "At About" ]
+                ]
+
+        NotFoundRoute ->
+            ul []
+                [ li [] [ text "Dashboard" ]
+                , li [] [ text "About" ]
+                ]
