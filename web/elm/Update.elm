0d663aad9fac23eb973a575b29b42f5c2fff573e
Wiwatta  Mongkhonchit <zentetsukenz@gmail.com>
2016-11-12 16:34:49 +0700

    Navigate to proper module

--- web/elm/Update.elm
+++ web/elm/Update.elm
@@ -3,16 +3,24 @@ module Update exposing (update)
 import Messages exposing (..)
 import Models exposing (..)
 import Routing exposing (..)
+import Navigation exposing (..)
 import NavBar.Update exposing (..)
+import NavBar.Messages exposing (..)
 
 
-update : Msg -> Model -> ( Model, Cmd Msg )
+update : Messages.Msg -> Model -> ( Model, Cmd Messages.Msg )
 update message model =
     case message of
         NavBarMsg navbarMessages ->
-            ( { model
-                | navBar =
-                    NavBar.Update.update navbarMessages model.navBar
-              }
-            , Cmd.none
-            )
+            let
+                moduleName =
+                    case navbarMessages of
+                        NavMsg identifier _ ->
+                            identifier
+            in
+                ( { model
+                    | navBar =
+                        NavBar.Update.update navbarMessages model.navBar
+                  }
+                , Navigation.newUrl ("#" ++ moduleName)
+                )
